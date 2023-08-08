8b436c29928de63633df6db3994283cdc2fe0077
Shixuanliu <420199938@qq.com>
2023-08-08 19:32:56 +0800

    Initial commit

---
+++ src/main/java/com/jiawa/mytrain/MyTrainApplication.java
@@ -0,0 +1,13 @@
+package com.jiawa.mytrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTrainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyTrainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jiawa/mytrain/MyTrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiawa.mytrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTrainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
