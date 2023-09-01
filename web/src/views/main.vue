db13f745d45ab25d4b9e5f041133dda55fdf709f
Shixuanliu <420199938@qq.com>
2023-09-01 08:59:56 +0800

    增加 the-sider.vue 组件

--- web/src/views/main.vue
+++ web/src/views/main.vue
@@ -2,64 +2,12 @@
   <a-layout id="components-layout-demo-top-side-2">
     <the-header-view></the-header-view>
     <a-layout>
-      <a-layout-sider width="200" style="background: #fff">
-        <a-menu
-            v-model:selectedKeys="selectedKeys2"
-            v-model:openKeys="openKeys"
-            mode="inline"
-            :style="{ height: '100%', borderRight: 0 }"
-        >
-          <a-sub-menu key="sub1">
-            <template #title>
-              <span>
-                <user-outlined />
-                subnav 1
-              </span>
-            </template>
-            <a-menu-item key="1">option1</a-menu-item>
-            <a-menu-item key="2">option2</a-menu-item>
-            <a-menu-item key="3">option3</a-menu-item>
-            <a-menu-item key="4">option4</a-menu-item>
-          </a-sub-menu>
-          <a-sub-menu key="sub2">
-            <template #title>
-              <span>
-                <laptop-outlined />
-                subnav 2
-              </span>
-            </template>
-            <a-menu-item key="5">option5</a-menu-item>
-            <a-menu-item key="6">option6</a-menu-item>
-            <a-menu-item key="7">option7</a-menu-item>
-            <a-menu-item key="8">option8</a-menu-item>
-          </a-sub-menu>
-          <a-sub-menu key="sub3">
-            <template #title>
-              <span>
-                <notification-outlined />
-                subnav 3
-              </span>
-            </template>
-            <a-menu-item key="9">option9</a-menu-item>
-            <a-menu-item key="10">option10</a-menu-item>
-            <a-menu-item key="11">option11</a-menu-item>
-            <a-menu-item key="12">option12</a-menu-item>
-          </a-sub-menu>
-        </a-menu>
-      </a-layout-sider>
-      <a-layout style="padding: 0 24px 24px">
-<!--        面包屑导航-->
-        <a-breadcrumb style="margin: 16px 0">
-          <a-breadcrumb-item>Home</a-breadcrumb-item>
-          <a-breadcrumb-item>List</a-breadcrumb-item>
-          <a-breadcrumb-item>App</a-breadcrumb-item>
-        </a-breadcrumb>
-        <a-layout-content
-            :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
-        >
-          Content
-        </a-layout-content>
-      </a-layout>
+      <the-sider-view></the-sider-view>
+      <a-layout-content
+          :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+      >
+        Content
+      </a-layout-content>
     </a-layout>
   </a-layout>
 </template>
@@ -67,9 +15,13 @@
 
 import {defineComponent} from "vue";
 import TheHeaderView from "@/components/the-header.vue";
+import TheSiderView from "@/components/the-sider.vue";
 
 export default defineComponent({
-  components: {TheHeaderView}
+  components: {
+    TheSiderView,
+    TheHeaderView,
+  }
 })
 </script>
 <style scoped>
