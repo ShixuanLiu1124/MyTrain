cbc05cdbb7de17b40c3e89d9d8c730f25741eb31
Shixuanliu <420199938@qq.com>
2023-09-05 16:41:25 +0800

    使用线程本地变量存储会员信息

---
+++ member/src/main/java/com/jiawa/mytrain/member/config/SpringMvcConfig.java
@@ -0,0 +1,32 @@
+package com.jiawa.mytrain.member.config;
+
+import com.jiawa.mytrain.common.interceptor.LogInterceptor;
+import com.jiawa.mytrain.common.interceptor.MemberInterceptor;
+import jakarta.annotation.Resource;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+   @Resource
+   LogInterceptor logInterceptor;
+
+   @Resource
+   MemberInterceptor memberInterceptor;
+
+   @Override
+   public void addInterceptors(InterceptorRegistry registry) {
+       registry.addInterceptor(logInterceptor);
+
+       // 路径不要包含context-path
+       registry.addInterceptor(memberInterceptor)
+               .addPathPatterns("/**") // 针对所有请求接口生效
+               .excludePathPatterns(
+                       "/hello",
+                       "/member/send-code",
+                       "/member/login"
+               );
+   }
+}
--- member/src/main/java/com/jiawa/mytrain/member/service/PassengerService.java
+++ member/src/main/java/com/jiawa/mytrain/member/service/PassengerService.java
@@ -2,6 +2,7 @@ package com.jiawa.mytrain.member.service;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
+import com.jiawa.mytrain.common.context.LoginMemberContext;
 import com.jiawa.mytrain.common.util.SnowUtil;
 import com.jiawa.mytrain.member.domain.Passenger;
 import com.jiawa.mytrain.member.mapper.PassengerMapper;
@@ -20,6 +21,7 @@ public class PassengerService {
     public void save(@Valid @RequestBody PassengerSaveReq req){
         DateTime now = DateTime.now();
         Passenger passenger = BeanUtil.copyProperties(req, Passenger.class);
+        passenger.setMemberId(LoginMemberContext.getId());
         passenger.setId(SnowUtil.getSnowflakeNextId());
         passenger.setCreateTime(now);
         passenger.setUpdateTime(now);
