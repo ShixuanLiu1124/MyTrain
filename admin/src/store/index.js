6b57b2b7296fd2783f525e352c899b9aedb8283d
Shixuanliu <420199938@qq.com>
2023-09-09 13:04:14 +0800

    修改管理员控台界面

--- admin/src/store/index.js
+++ admin/src/store/index.js
@@ -1,21 +1,9 @@
 import {createStore} from 'vuex'
 
-const MEMBER = 'member';
-
 export default createStore({
-    state: {
-        // || {} 避免空指针异常
-        member: window.SessionStorage.get(MEMBER) || {},
-    },
+    state: {},
     getters: {},
-    mutations: {
-        setMember(state, _member) {
-            state.member = _member;
-            window.SessionStorage.set(MEMBER, _member);
-        }
-    },
-    actions: {
-
-    },
+    mutations: {},
+    actions: {},
     modules: {}
 })
